b5e26b19d8e72dacb65e3562b1667e1c7d853389
Bob McClellan Jr <mcclellan@chgr.mc.vanderbilt.edu>
2011-03-16 13:55:02 -0500

    Boilerplate app creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwHome::Application.config.secret_token = 'e6410d2e4894937ecfaf8c490ab9bdbc78dfbf014d2caafd1d173b8f812b83f5fe3510389c5bff89ccfa365f8be19ef51a882cad5dd6af386386534af6372998'
